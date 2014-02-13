7b3acd305c4a947ab670efb77c86ade6039e4342
Richard Williams <coldheart042@gmail.com>
2014-02-12 16:34:51 -0800

    Revision 2
    Wrote Customer Object constructors

--- cookNook/cookNook/CustomerObj.cs
+++ cookNook/cookNook/CustomerObj.cs
@@ -7,5 +7,15 @@ namespace cookNook
 {
     public class CustomerObj
     {
+        public String strNameFirst { get; set; }
+        public String strNameLast { get; set; }
+        public String strAddress { get; set; }
+        public String strCity { get; set; }
+        public String strState { get; set; }
+        public String strZip { get; set; }
+        public String strPassword { get; set; }
+        public String strEmail { get; set; }
+        public String strPhone { get; set; }
+        public String CustomerDate { get; set; }
     }
 }
